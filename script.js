5bbbfc57e5cc5308e1c303e400a5fe8ad94b76ac
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 23:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-function dtb(decimal)
-	{
-		let binary='';
-
-		while(decimal>0)
-			{
-				const remainder=decimal%2;
-				binary+=remainder;
-				decimal=Math.floor(decimal/2);
-			}
-		return binary.split('').reverse().join('');
-	
-	}
-module.exports=dtb();
+function threeSum(arr, target) {
+ let binaryString = "";
+  while (arr > 0) {
+    binaryString = (arr % 2) + binaryString;
+    arr = Math.floor(arr / 2);
+  }
+  return binaryString;
+}
+module.exports = threeSum;
