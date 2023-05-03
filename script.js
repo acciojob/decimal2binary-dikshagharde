7bebf25d93659c9ae2f588a60fc6b9deb041c0a8
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 23:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 function threeSum(arr, target) {
- let binaryString = "";
-  while (arr > 0) {
-    binaryString = (arr % 2) + binaryString;
-    arr = Math.floor(arr / 2);
-  }
-  return binaryString;
+// write your code 
+	 // array to store binary number
+    let binaryNum = new Array(32);
+ 
+    // counter for binary array
+    let i = 0;
+    while (arr > 0) {
+ 
+        // storing remainder in binary array
+        binaryNum[i] = arr % 2;
+        arr = Math.floor(arr / 2);
+        i++;
+    }
+ 
+    // printing binary array in reverse order
+    for (let j = i - 1; j >= 0; j--)
+        document.write(binaryNum[j]);
 }
+ 
+// Driver program to test above function
+    let arr = 17;
+    decToBinary(arr);
+  
+}
+
 module.exports = threeSum;
