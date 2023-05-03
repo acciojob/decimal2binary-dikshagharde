5339665e3a5fb37e6234fe7955031f8827c6c346
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 23:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-function decimalToBinary(decimalNumber) {
-  // initialize variables
-  let binaryNumber = "";
-  let quotient = decimalNumber;
+function dtb(decimal)
+	{
+		let binary='';
 
-  // perform division until quotient is zero
-  while (quotient !== 0) {
-    // calculate remainder
-    let remainder = quotient % 2;
-
-    // add remainder to binary number
-    binaryNumber = remainder.toString() + binaryNumber;
-
-    // divide quotient by 2
-    quotient = Math.floor(quotient / 2);
-  }
-
-  // return binary number as a string
-  return binaryNumber;
-}
+		while(decimal>0)
+			{
+				const remainder=decimal%2;
+				binary+=remainder;
+				decimal=Math.floor(decimal/2);
+			}
+		return binary.split('').reverse().join('');
+	
+	}
+module.exports=dtb();
