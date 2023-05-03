4b9534f0b231649b36c51618ad5850b22feea63e
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 23:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const decimalToBinary = (num) => {
-  let binaryStr = num.toString(2);
-  return binaryStr;
-};
 
-const num = parseInt(process.argv[2]);
-const binaryStr = decimalToBinary(num);
-console.log(binaryStr);
+function decimalToBinary(num){
+    let binaryStr = ""
+
+    while(num>0){
+
+        let rem = num%2;
+
+        // Method 1 - Appending the new binary character to the front of the string
+        // binaryStr = rem.toString() + binaryStr
+
+        // Method 2 - Append it to the end
+        binaryStr = binaryStr + rem.toString()
+
+        num = Math.floor(num/2)
+    }
+    // Method - 1
+    // return binaryStr
+
+
+    // Method 2- Reverse it
+    return binaryStr.split("").reverse().join("")
+}
+
+module.exports = decimalToBinary;
