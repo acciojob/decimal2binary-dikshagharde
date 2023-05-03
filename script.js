592fd947df81a784960985effc6134ae0aed22fd
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 23:03:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function binary(){
+	let val = document.getElementById("decimalNumber").value;
+
+	let binarynumber="";
+
+	while(val>0){
+		var remainder = val%2;
+		binarynumber = remainder + binarynumber;
+		val = Math.floor(val/2);
+	}
+
+	console.log(binarynumber);
 }
+
 
 module.exports = threeSum;
